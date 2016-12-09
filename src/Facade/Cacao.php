1e210f222eb758cd0e5cc01229da54308fafe1ee
bigfaster <desarrollo@bigfaster.com>
2016-12-09 08:10:04 -0500

    Facade Cacao

---
+++ src/Facade/Cacao.php
@@ -0,0 +1,9 @@
+<?php namespace Kodazzi\CacaoBase\Facade;
+
+use Illuminate\Support\Facades\Facade;
+
+class Cacao extends Facade {
+
+    protected static function getFacadeAccessor() { return 'cacao'; }
+
+}
